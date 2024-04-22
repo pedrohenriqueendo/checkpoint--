063c3b457cfb13d347c98e9a715f9479b65c046a
pedrohenriqueendo <pedrohenrique.endo@hotmail.com>
2024-04-22 12:10:10 -0300

    projeto finalizado

--- screens/overview.tsx
+++ screens/overview.tsx
@@ -1,39 +1,68 @@
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { ScreenContent } from 'components/ScreenContent';
-import { StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  Image,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
 
+const Overview = ({ navigation }) => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(
+          'https://raw.githubusercontent.com/erik-sytnyk/movies-list/master/db.json'
+        );
+        const json = await response.json();
+        setMovies(json.movies);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-import { Button } from '../components/Button';
-import { RootStackParamList } from '../navigation';
+    fetchMovies();
+  }, []);
 
-type OverviewScreenNavigationProps = StackNavigationProp<RootStackParamList, 'Overview'>;
-
-export default function Overview() {
-  const navigation = useNavigation<OverviewScreenNavigationProps>();
+  if (loading) {
+    return <ActivityIndicator size="large" />;
+  }
 
   return (
-    <View style={styles.container}>
-      <ScreenContent path="screens/overview.tsx" title="Overview">
-      
-      </ScreenContent>
-      <Button
-        onPress={() =>
-          navigation.navigate('Details', {
-            name: 'Dan',
-          })
-        }
-        title="Show Details"
-      />
-    </View>
+    <FlatList
+      data={movies}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          style={styles.movieContainer}
+          onPress={() => navigation.navigate('Details', { movieDetails: item })}>
+          {item.posterUrl ? <Image source={{ uri: item.posterUrl }} style={styles.image} /> : null}
+          <Text style={styles.text}>{item.title}</Text>
+        </TouchableOpacity>
+      )}
+    />
   );
-}
+};
 
+export default Overview;
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
+const styles = StyleSheet.create({
+  movieContainer: {
+    marginHorizontal: 32,
+    marginVertical: 10,
+    padding: 16,
+    borderColor: 'black',
+    borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
+  image: { width: 100, height: 100 },
+  text: { marginLeft: 8 },
 });
